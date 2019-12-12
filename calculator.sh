dcc3f6608195927f1484508145c0381cecf22a96
Sid Lute <lutesid@gmail.com>
2019-12-12 03:01:00 +0000

    added calculator

---
+++ calculator.sh
@@ -0,0 +1,12 @@
+calci(){
+	case $3 in
+		add) var=$(echo "$1+$2"|bc); echo "The sum of 2 numbers is $var";;
+		sub) var=$(echo "$1-$2"|bc); echo "The subtraction of 2 numbers is $var";;
+		mul) var=$(echo "$1*$2"|bc); echo "The product of 2 numbers is $var";;
+		div) var=$(echo "$1/$2"|bc); echo "The division of 2 numbers is $var";;
+		*) echo "Enter correct values please";;
+	esac
+}
+read -p "Welcome to the calculator. Please enter the numbers in format as Num1 Num2 Operation " x y z
+
+calci $x $y $z
